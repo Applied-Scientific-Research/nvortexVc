9309d69522c317636c56b29110a87c2a09d20d47
Mark Stock <markjstock@gmail.com>
2022-09-20 13:37:48 -0400

    Serial 3d is fully-MPI-ized

--- src/nvortex3d.cpp
+++ src/nvortex3d.cpp
@@ -12,10 +12,6 @@
  *   ./nvortex3d -n=10000 8 2
  */
 
-#ifdef USE_VC
-#include <Vc/Vc>
-#endif
-
 #include <cstdlib>
 #include <cstdio>
 #include <cstring>
@@ -24,11 +20,14 @@
 #include <algorithm>
 #include <random>
 #include <chrono>
+
 #ifdef USE_MPI
 #include <mpi.h>
 #endif
 
 #ifdef USE_VC
+#include <Vc/Vc>
+
 using Vc::float_v;
 using VectorF = std::vector<float, Vc::Allocator<float>>;
 #endif
@@ -61,9 +60,6 @@ void nbody_serial(const int numSrcs, const float* const __restrict__ sx, const f
 
     #pragma omp parallel for
     for (int i = 0; i < numTarg; i++) {
-        tax[i] = 0.0;
-        tay[i] = 0.0;
-        taz[i] = 0.0;
         for (int j = 0; j < numSrcs; j++) {
             nbody_kernel_serial(sx[j], sy[j], sz[j], ssx[j], ssy[j], ssz[j], sr[j],
                                 tx[i], ty[i], tz[i], tr[i], &tax[i], &tay[i], &taz[i]);
@@ -255,18 +251,137 @@ inline Vc::float_v* floatarry_to_floatvarry (const float* const in, const int n,
 }
 #endif
 
+// not really alignment, just minimum block sizes
+int buffer(const int _n, const int _align) {
+  // 63,64 returns 1*64; 64,64 returns 1*64; 65,64 returns 2*64=128
+  return _align*((_n+_align-1)/_align);
+}
 
-// main program
+// distribute ntotal items across nproc processes, now how many are on proc iproc?
+int nthisproc(const int ntotal, const int iproc, const int nproc) {
+  const int base_nper = ntotal / nproc;
+  return (iproc < (ntotal-base_nper*nproc)) ? base_nper+1 : base_nper;
+}
+
+// simple struct to hold sources
+struct Sources {
+
+  int n = 0;
+  float* x = nullptr;
+  float* y = nullptr;
+  float* z = nullptr;
+  float* r = nullptr;
+  float* sx = nullptr;
+  float* sy = nullptr;
+  float* sz = nullptr;
+
+  // sized constructor
+  Sources(const int _n) : n(_n) {
+    x = new float[_n];
+    y = new float[_n];
+    z = new float[_n];
+    r = new float[_n];
+    sx = new float[_n];
+    sy = new float[_n];
+    sz = new float[_n];
+  }
+
+  // default constructor - keep pointers null
+  Sources() {}
+
+  // copy constructor
+  Sources(const Sources& _s) : Sources(_s.n) {
+    deep_copy(_s);
+  }
+
+  void init_rand(std::mt19937 _gen) {
+    std::uniform_real_distribution<> zmean_dist(-1.0, 1.0);
+    for (int i=0; i<n; ++i) x[i] = zmean_dist(_gen);
+    for (int i=0; i<n; ++i) y[i] = zmean_dist(_gen);
+    for (int i=0; i<n; ++i) z[i] = zmean_dist(_gen);
+    for (int i=0; i<n; ++i) sx[i] = zmean_dist(_gen);
+    for (int i=0; i<n; ++i) sy[i] = zmean_dist(_gen);
+    for (int i=0; i<n; ++i) sz[i] = zmean_dist(_gen);
+    const float rad = 1.0 / std::sqrt((float)n);
+    for (int i=0; i<n; ++i) r[i] = rad;
+  }
+
+  void deep_copy(const Sources& _s) {
+    assert(n == _s.n && "Copying from Sources with different n");
+    for (int i=0; i<n; ++i) x[i] = _s.x[i];
+    for (int i=0; i<n; ++i) y[i] = _s.y[i];
+    for (int i=0; i<n; ++i) z[i] = _s.z[i];
+    for (int i=0; i<n; ++i) r[i] = _s.r[i];
+    for (int i=0; i<n; ++i) sx[i] = _s.sx[i];
+    for (int i=0; i<n; ++i) sy[i] = _s.sy[i];
+    for (int i=0; i<n; ++i) sz[i] = _s.sz[i];
+  }
+
+  void shallow_copy(const Sources& _s) {
+    // warning: this can orphan memory!!!
+    n = _s.n;
+    x = _s.x;
+    y = _s.y;
+    z = _s.z;
+    r = _s.r;
+    sx = _s.sx;
+    sy = _s.sy;
+    sz = _s.sz;
+  }
+
+  // destructor
+  ~Sources() {
+    //if (x != nullptr) delete x;
+    //if (y != nullptr) delete y;
+    //if (z != nullptr) delete z;
+    //if (r != nullptr) delete r;
+    //if (sx != nullptr) delete sx;
+    //if (sy != nullptr) delete sy;
+    //if (sz != nullptr) delete sz;
+  }
+};
+
+#ifdef USE_MPI
+// non-class method to swap sources
+void exchange_sources(const Sources& sendsrcs, const int ito,
+                            Sources& recvsrcs, const int ifrom,
+                            MPI_Request* handle) {
+
+  // how many to send and receive?
+  MPI_Sendrecv(&sendsrcs.n, 1, MPI_INT, ito, 9, &recvsrcs.n, 1, MPI_INT, ifrom, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+  //std::cout << "  proc " << iproc << " sending " << sendsrcs.n << " and receiving " << recvsrcs.n << std::endl;
+
+  // do the transfers, saving the handle for future reference
+  MPI_Irecv(recvsrcs.x, recvsrcs.n, MPI_FLOAT, ifrom, 10, MPI_COMM_WORLD, &handle[0]);
+  MPI_Isend(sendsrcs.x, sendsrcs.n, MPI_FLOAT, ito, 10, MPI_COMM_WORLD, &handle[1]);
+  MPI_Irecv(recvsrcs.y, recvsrcs.n, MPI_FLOAT, ifrom, 11, MPI_COMM_WORLD, &handle[2]);
+  MPI_Isend(sendsrcs.y, sendsrcs.n, MPI_FLOAT, ito, 11, MPI_COMM_WORLD, &handle[3]);
+  MPI_Irecv(recvsrcs.z, recvsrcs.n, MPI_FLOAT, ifrom, 12, MPI_COMM_WORLD, &handle[4]);
+  MPI_Isend(sendsrcs.z, sendsrcs.n, MPI_FLOAT, ito, 12, MPI_COMM_WORLD, &handle[5]);
+  MPI_Irecv(recvsrcs.r, recvsrcs.n, MPI_FLOAT, ifrom, 13, MPI_COMM_WORLD, &handle[6]);
+  MPI_Isend(sendsrcs.r, sendsrcs.n, MPI_FLOAT, ito, 13, MPI_COMM_WORLD, &handle[7]);
+  MPI_Irecv(recvsrcs.sx, recvsrcs.n, MPI_FLOAT, ifrom, 14, MPI_COMM_WORLD, &handle[8]);
+  MPI_Isend(sendsrcs.sx, sendsrcs.n, MPI_FLOAT, ito, 14, MPI_COMM_WORLD, &handle[9]);
+  MPI_Irecv(recvsrcs.sy, recvsrcs.n, MPI_FLOAT, ifrom, 15, MPI_COMM_WORLD, &handle[10]);
+  MPI_Isend(sendsrcs.sy, sendsrcs.n, MPI_FLOAT, ito, 15, MPI_COMM_WORLD, &handle[11]);
+  MPI_Irecv(recvsrcs.sz, recvsrcs.n, MPI_FLOAT, ifrom, 16, MPI_COMM_WORLD, &handle[12]);
+  MPI_Isend(sendsrcs.sz, sendsrcs.n, MPI_FLOAT, ito, 16, MPI_COMM_WORLD, &handle[13]);
+}
+#endif
 
 static void usage() {
     fprintf(stderr, "Usage: nbody [-n=<number>] [simd iterations] [serial iterations]\n");
     exit(1);
 }
 
+// main program
+
 int main(int argc, char *argv[]) {
 
     static unsigned int test_iterations[] = {4, 4, 4, 2};
     int n_in = 10000;
+    int nproc = 1;
+    int iproc = 0;
 
     if (argc > 1) {
         if (strncmp(argv[1], "-n=", 3) == 0) {
@@ -286,19 +401,22 @@ int main(int argc, char *argv[]) {
 
 #ifdef USE_MPI
     (void) MPI_Init(&argc, &argv);
-
-    int nproc = -1;
     (void) MPI_Comm_size(MPI_COMM_WORLD, &nproc);
-    int iproc = -1;
     (void) MPI_Comm_rank(MPI_COMM_WORLD, &iproc);
     if (iproc==0) std::cout << "MPI-capable binary" << std::endl;
-    std::cout << "Proc " << iproc << " of " << nproc << std::endl;
+
+    // user specifies *total* number of particles, each processor take a share of those
+    n_in = nthisproc(n_in, iproc, nproc);
+
+    std::cout << "Proc " << iproc << " of " << nproc << " owns " << n_in << " particles" << std::endl;
+#else
+    std::cout << "Running with " << n_in << " particles" << std::endl;
 #endif
 
     // set problem size
     const int maxGangSize = 16;		// this is 512 bits / 32 bits per float
-    int numSrcs = maxGangSize*(n_in/maxGangSize);
-    int numTargs = maxGangSize*(n_in/maxGangSize);
+    const int numSrcs = buffer(n_in,maxGangSize);
+    const int numTargs = numSrcs;
 
     // init random number generator
     std::random_device rd;  //Will be used to obtain a seed for the random number engine
@@ -306,23 +424,26 @@ int main(int argc, char *argv[]) {
     std::uniform_real_distribution<> zmean_dist(-1.0, 1.0);
 
     // allocate original particle data (used for x86 reference calculation)
+    Sources src = Sources(numSrcs);
+    src.init_rand(gen);
 
-    float* sx = new float[numSrcs];
-    float* sy = new float[numSrcs];
-    float* sz = new float[numSrcs];
-    float* ssx = new float[numSrcs];
-    float* ssy = new float[numSrcs];
-    float* ssz = new float[numSrcs];
-    float* sr = new float[numSrcs];
-    for (int i = 0; i < numSrcs; i++) {
-        sx[i] = zmean_dist(gen);
-        sy[i] = zmean_dist(gen);
-        sz[i] = zmean_dist(gen);
-        ssx[i] = zmean_dist(gen);
-        ssy[i] = zmean_dist(gen);
-        ssz[i] = zmean_dist(gen);
-        sr[i] = 1.0 / std::sqrt((float)numSrcs);
-    }
+#ifdef USE_MPI
+    // what's the largest number that we'll encounter?
+    int n_large = 0;
+    MPI_Allreduce(&numSrcs, &n_large, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
+    // make two sets of arrays for communication
+    Sources work = Sources(n_large);
+    Sources buf = Sources(n_large);
+    // and one set of pointers
+    Sources ptr;
+
+    // always pull from left (-), send to right (+)
+    const int ifrom = (iproc == 0) ? nproc-1 : iproc-1;
+    const int ito = (iproc == nproc-1) ? 0 : iproc+1;
+    MPI_Request* xfers = new MPI_Request[14];
+    for (int ix=0; ix<14; ++ix) xfers[ix] = MPI_REQUEST_NULL;
+    //std::cout << "  proc " << iproc << " always sending to " << ito << " and receiving from " << ifrom << std::endl;
+#endif
 
     float* tx = new float[numTargs];
     float* ty = new float[numTargs];
@@ -345,22 +466,22 @@ int main(int argc, char *argv[]) {
 
 #ifdef USE_VC
     // vectorize over arrays of float_v types
-    Vc::float_v* vsx = floatarry_to_floatvarry(sx, numSrcs, 0.0);
-    Vc::float_v* vsy = floatarry_to_floatvarry(sy, numSrcs, 0.0);
-    Vc::float_v* vsz = floatarry_to_floatvarry(sz, numSrcs, 0.0);
-    Vc::float_v* vssx = floatarry_to_floatvarry(ssx, numSrcs, 0.0);
-    Vc::float_v* vssy = floatarry_to_floatvarry(ssy, numSrcs, 0.0);
-    Vc::float_v* vssz = floatarry_to_floatvarry(ssz, numSrcs, 0.0);
-    Vc::float_v* vsr = floatarry_to_floatvarry(sr, numSrcs, 1.0);
+    Vc::float_v* vsx = floatarry_to_floatvarry(src.x, numSrcs, 0.0);
+    Vc::float_v* vsy = floatarry_to_floatvarry(src.y, numSrcs, 0.0);
+    Vc::float_v* vsz = floatarry_to_floatvarry(src.z, numSrcs, 0.0);
+    Vc::float_v* vssx = floatarry_to_floatvarry(src.sx, numSrcs, 0.0);
+    Vc::float_v* vssy = floatarry_to_floatvarry(src.sy, numSrcs, 0.0);
+    Vc::float_v* vssz = floatarry_to_floatvarry(src.sz, numSrcs, 0.0);
+    Vc::float_v* vsr = floatarry_to_floatvarry(src.r, numSrcs, 1.0);
 
     // vectorize over standard library vector objects
-    VectorF svsx(sx, sx+numSrcs);
-    VectorF svsy(sy, sy+numSrcs);
-    VectorF svsz(sz, sz+numSrcs);
-    VectorF svssx(ssx, ssx+numSrcs);
-    VectorF svssy(ssy, ssy+numSrcs);
-    VectorF svssz(ssz, ssz+numSrcs);
-    VectorF svsr(sr, sr+numSrcs);
+    VectorF svsx(src.x, src.x+numSrcs);
+    VectorF svsy(src.y, src.y+numSrcs);
+    VectorF svsz(src.z, src.z+numSrcs);
+    VectorF svssx(src.sx, src.sx+numSrcs);
+    VectorF svssy(src.sy, src.sy+numSrcs);
+    VectorF svssz(src.sz, src.sz+numSrcs);
+    VectorF svsr(src.r, src.r+numSrcs);
 
 
     //
@@ -377,13 +498,15 @@ int main(int argc, char *argv[]) {
         minVc = std::min(minVc, elapsed_seconds.count());
     }
 
-    if (test_iterations[0] > 0) {
+    if (test_iterations[0] > 0 and iproc==0) {
     printf("[nbody Vc 01]:\t\t[%.6f] seconds\n", minVc);
     printf("              \t\t[%.6f] GFlop/s\n", (float)numSrcs*numTargs*num_flops_per/(1.e+9*minVc));
 
     // Write sample results
-    for (int i = 0; i < 2; i++) printf("   particle %d vel %g %g %g\n",i,tax[i],tay[i],taz[i]);
-    printf("\n");
+    if (iproc==0) {
+        for (int i = 0; i < 2; i++) printf("   particle %d vel %g %g %g\n",i,tax[i],tay[i],taz[i]);
+        printf("\n");
+    }
     }
 
 
@@ -401,13 +524,15 @@ int main(int argc, char *argv[]) {
         minVc02 = std::min(minVc02, elapsed_seconds.count());
     }
 
-    if (test_iterations[1] > 0) {
+    if (test_iterations[1] > 0 and iproc==0) {
     printf("[nbody Vc 02]:\t\t[%.6f] seconds\n", minVc02);
     printf("              \t\t[%.6f] GFlop/s\n", (float)numSrcs*numTargs*num_flops_per/(1.e+9*minVc02));
 
     // Write sample results
-    for (int i = 0; i < 2; i++) printf("   particle %d vel %g %g %g\n",i,tax[i],tay[i],taz[i]);
-    printf("\n");
+    if (iproc==0) {
+        for (int i = 0; i < 2; i++) printf("   particle %d vel %g %g %g\n",i,tax[i],tay[i],taz[i]);
+        printf("\n");
+    }
 
     // accumulate minimum
     minVc = std::min(minVc, minVc02);
@@ -428,13 +553,15 @@ int main(int argc, char *argv[]) {
         minVc03 = std::min(minVc03, elapsed_seconds.count());
     }
 
-    if (test_iterations[2] > 0) {
+    if (test_iterations[2] > 0 and iproc==0) {
     printf("[nbody Vc 03]:\t\t[%.6f] seconds\n", minVc03);
     printf("              \t\t[%.6f] GFlop/s\n", (float)numSrcs*numTargs*num_flops_per/(1.e+9*minVc03));
 
     // Write sample results
-    for (int i = 0; i < 2; i++) printf("   particle %d vel %g %g %g\n",i,tax[i],tay[i],taz[i]);
-    printf("\n");
+    if (iproc==0) {
+        for (int i = 0; i < 2; i++) printf("   particle %d vel %g %g %g\n",i,tax[i],tay[i],taz[i]);
+        printf("\n");
+    }
 
     // accumulate minimum
     minVc = std::min(minVc, minVc03);
@@ -451,21 +578,60 @@ int main(int argc, char *argv[]) {
     double minSerial = 1e30;
     for (unsigned int i = 0; i < test_iterations[3]; ++i) {
         auto start = std::chrono::system_clock::now();
-        nbody_serial(numSrcs, sx, sy, sz, ssx, ssy, ssz, sr,
+
+        // zero the vels
+        for (int j=0; j<numTargs; ++j) tax[j] = 0.0;
+        for (int j=0; j<numTargs; ++j) tay[j] = 0.0;
+        for (int j=0; j<numTargs; ++j) taz[j] = 0.0;
+
+#ifdef USE_MPI
+        // first set to compute is self
+        ptr.shallow_copy(src);
+
+        for (int ibatch=0; ibatch<nproc ; ++ibatch) {
+            // post non-blocking sends (from ptr) and receives (into buf)
+            if (ibatch < nproc-1) {
+                exchange_sources(ptr, ito, buf, ifrom, xfers);
+            }
+
+            // run the O(N^2) calculation concurrently using ptr for sources
+            nbody_serial(ptr.n, ptr.x, ptr.y, ptr.z, ptr.sx, ptr.sy, ptr.sz, ptr.r,
+                         numTargs, tx, ty, tz, tr, tax, tay, taz);
+
+            // wait for new data to arrive before continuing
+            if (ibatch < nproc-1) {
+                // wait on all 14 transfers to complete
+                MPI_Waitall(14, xfers, MPI_STATUS_IGNORE);
+                // copy buf to work
+                work.deep_copy(buf);
+                // ptr now points at work
+                ptr.shallow_copy(work);
+            }
+        }
+
+        // and to ensure correct timings, wait for all MPI processes to finish
+        MPI_Barrier(MPI_COMM_WORLD);
+#else
+        // run the O(N^2) calculation
+        nbody_serial(numSrcs, src.x, src.y, src.z, src.sx, src.sy, src.sz, src.r,
                      numTargs, tx, ty, tz, tr, tax, tay, taz);
+#endif
+
         auto end = std::chrono::system_clock::now();
         std::chrono::duration<double> elapsed_seconds = end-start;
-        printf("@time of serial run:\t\t\t[%.6f] seconds\n", (float)elapsed_seconds.count());
+        if (iproc==0) printf("@time of serial run:\t\t\t[%.6f] seconds\n", (float)elapsed_seconds.count());
         minSerial = std::min(minSerial, elapsed_seconds.count());
     }
 
-    if (test_iterations[3] > 0) {
+    if (test_iterations[3] > 0 and iproc==0) {
     printf("[nbody serial]:\t\t[%.6f] seconds\n", minSerial);
-    printf("               \t\t[%.6f] GFlop/s\n", (float)numSrcs*numTargs*num_flops_per/(1.e+9*minSerial));
+    printf("               \t\t[%.6f] GFlop/s\n", (float)numSrcs*numTargs*num_flops_per*nproc*nproc/(1.e+9*minSerial));
 
     // Write sample results
-    for (int i = 0; i < 2; i++) printf("   particle %d vel %g %g %g\n",i,tax[i],tay[i],taz[i]);
-    printf("\n");
+    if (iproc==0) {
+        for (int i=0; i<2; i++) printf("   particle %d vel %g %g %g\n",i,tax[i],tay[i],taz[i]);
+        printf("\n");
+    }
 
 #ifdef USE_VC
     // calculate error estimate
